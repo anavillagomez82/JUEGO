c5b7a9c6f3dabdef756250159bfc0cdc0e7bc7ea
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-11-24 16:22:36 -0500

    Update Enemie.h

--- Enemie.h
+++ Enemie.h
@@ -1,14 +1,12 @@
-#include <bits/stdc++.h> 
-#include <SFML/Graphics.hpp>
 #ifndef ENEMIE_H
 #define ENEMIE_H
+
+#include <SFML/Graphics.hpp>
 #include "GameObject.h"
-using namespace std;
-using namespace sf;
 
 class Enemie : public GameObject {
 public:
-    Enemie(sf ::Vector2f position, sf::Texture& texture);
+    Enemie(sf::Vector2f position, sf::Texture& texture);
     void Update(sf::Vector2f playerPosition, int dist) override; // Implementación del método Update
     sf::Vector2f GetPosition() const override; // Implementación del método GetPosition
     virtual void draw(sf::RenderTarget&, sf::RenderStates) const override;
