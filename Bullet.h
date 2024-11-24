1cea66cb0e7908ced88562d4d9927f16705b18ed
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-11-23 22:15:23 -0500

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -1,21 +1,21 @@
-#include <bits/stdc++.h>
-#include <SFML/Graphics.hpp>
 #ifndef BULLET_H
 #define BULLET_H
-
+#include <bits/stdc++.h>
+#include <SFML/Graphics.hpp>
+#include "GameObject.h"
 using namespace std;
 using namespace sf;
 
-class  Bullet : public Drawable{
-    public:
-        Bullet(Vector2f,Vector2f,float,Texture&);
-        void Update();
-        Vector2f GetPosition();
-        virtual void draw(RenderTarget&,RenderStates) const;
-    private:
-        Sprite sprite;
-        float vel;
-        Vector2f dir;
-        void Movement();
+class Bullet : public GameObject {
+public:
+    Bullet(sf::Vector2f position, sf::Vector2f direction, float rotation, sf::Texture& texture);
+    void Update() override; // Implementación del método Update
+    sf::Vector2f GetPosition() const override; // Implementación del método GetPosition
+    virtual void draw(sf::RenderTarget&, sf::RenderStates) const override;
+
+private:
+    sf::Sprite sprite;
+    sf::Vector2f velocity;
 };
-#endif
+
+#endif // BULLET_H
