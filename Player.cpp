07a7fbab89887cdd0feb5dbeb14e5f1bc8d34509
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-11-23 22:12:10 -0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,65 +1,68 @@
 #include "Player.h"
+#include "GameObject.h"
 
-Player :: Player(Texture &texture){
+Player::Player(sf::Texture &texture) {
     sprite.setTexture(texture);
-    sprite.setTextureRect(IntRect(0,115,250,115));
-    sprite.setScale(0.35,0.35);
-    sprite.setOrigin((250/2)/2,115/2);
-    sprite.setPosition(0,0);
-    vel=2;
+    sprite.setTextureRect(sf::IntRect(0, 115, 250, 115));
+    sprite.setScale(0.35, 0.35);
+    sprite.setOrigin((250 / 2) / 2, 115 / 2);
+    sprite.setPosition(0, 0);
+    vel = 2;
 }
 
-void Player :: Update(RenderWindow &rw, View &view){
+void Player::Update(sf::RenderWindow &rw, sf::View &view) {
     Movement(view);
     Aim(rw);
 }
 
-void Player::Movement(View &view){
-    if(Keyboard::isKeyPressed(Keyboard::W)&&sprite.getPosition().y>-485){
-        sprite.move(0,-vel);
-        view.move(0,-vel);
+void Player::Movement(sf::View &view) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sprite.getPosition().y > -485) {
+        sprite.move(0, -vel);
+        view.move(0, -vel);
     }
-    if(Keyboard::isKeyPressed(Keyboard::S)&&sprite.getPosition().y<485){
-        sprite.move(0,vel);
-        view.move(0,vel);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && sprite.getPosition().y < 485) {
+        sprite.move(0, vel);
+        view.move(0, vel);
     }
-    if(Keyboard::isKeyPressed(Keyboard::D)&&sprite.getPosition().x<735){
-        sprite.move(vel,0);
-        view.move(vel,0);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && sprite.getPosition().x < 735) {
+        sprite.move(vel, 0);
+        view.move(vel, 0);
     }
-    if(Keyboard::isKeyPressed(Keyboard::A)&&sprite.getPosition().x>-735){
-        sprite.move(-vel,0);
-        view.move(-vel,0);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && sprite.getPosition().x > -735) {
+        sprite.move(-vel, 0);
+        view.move(-vel, 0);
     }
 }
 
-void Player::Aim(RenderWindow &rw){
-    sprite.setRotation(atan2(Mouse::getPosition(rw).y-rw.mapCoordsToPixel(sprite.getPosition()).y,Mouse::getPosition(rw).x-rw.mapCoordsToPixel(sprite.getPosition()).x)*180/3.14159265f);
+void Player::Aim(sf::RenderWindow &rw) {
+    sprite.setRotation(atan2(sf::Mouse::getPosition(rw).y - rw.mapCoordsToPixel(sprite.getPosition()).y, 
+                              sf::Mouse::getPosition(rw).x - rw.mapCoordsToPixel(sprite.getPosition()).x) * 180 / 3.14159265f);
 }
 
-pair<bool,Vector2f> Player::AnswerShoot(RenderWindow &rw){
-    Vector2f dir = Vector2f(Mouse::getPosition(rw))-Vector2f(rw.mapCoordsToPixel(sprite.getPosition()));
-    Vector2f dirNorm = Vector2f(dir.x/sqrt(pow(dir.x,2)+pow(dir.y,2)),dir.y/sqrt(pow(dir.x,2)+pow(dir.y,2)));
+std::pair<bool, sf::Vector2f> Player::AnswerShoot(sf::RenderWindow &rw) {
+    sf::Vector2f dir = sf::Vector2f(sf::Mouse::getPosition(rw)) - sf::Vector2f(rw.mapCoordsToPixel(sprite.getPosition()));
+    sf::Vector2f dirNorm = sf::Vector2f(dir.x / sqrt(pow(dir.x, 2) + pow(dir.y, 2)), 
+                                             dir.y / sqrt(pow(dir.x, 2) + pow(dir.y, 2)));
 
-    if(Mouse::isButtonPressed(Mouse::Left) && cadence >= 8){
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && cadence >= 8) {
         cadence = 1;
-        sprite.setTextureRect(IntRect(0,0,250,115));
-        return {true,dirNorm};
+        sprite.setTextureRect(sf::IntRect(0, 0, 250, 115));
+        return {true, dirNorm};
     }
     cadence++;
-    if(cadence==4) sprite.setTextureRect(IntRect(0,115,250,115));
+    if (cadence == 4) sprite.setTextureRect(sf::IntRect(0, 115, 250, 115));
 
-    return {false,dirNorm};
+    return {false, dirNorm};
 }
 
-float Player::GetRotation(){
+float Player::GetRotation() {
     return sprite.getRotation();
 }
 
-Vector2f Player::GetPosition(){
+sf::Vector2f Player::GetPosition() const {
     return sprite.getPosition();
 }
 
-void Player::draw(RenderTarget &rt, RenderStates rs) const{
-    rt.draw(sprite,rs);
+void Player::draw(sf::RenderTarget &rt, sf::RenderStates rs) const {
+    rt.draw(sprite, rs);
 }
