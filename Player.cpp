10c61f834000f4551c7035a55fa942d63188b042
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-11-26 17:29:02 -0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,68 +1,43 @@
 #include "Player.h"
 
-Player::Player(sf::Texture &texture) {
-    sprite.setTexture(texture);
-    sprite.setTextureRect(sf::IntRect(0, 115, 250, 115));
+Player::Player(Texture &texture) : Character(texture, Vector2f(0, 0)) {
+    sprite.setTextureRect(IntRect(0, 115, 250, 115));
     sprite.setScale(0.35, 0.35);
     sprite.setOrigin((250 / 2) / 2, 115 / 2);
     sprite.setPosition(0, 0);
-    vel = 2;
+    vel = 2.5;
+    Hp = 100; // Asignar HP inicial
 }
 
-void Player::Update(sf::RenderWindow &rw, sf::View &view) {
-    Movement(view);
-    Aim(rw);
-}
-
-void Player::Movement(sf::View &view) {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sprite.getPosition().y > -485) {
+void Player::Update(RenderWindow &rw, View &view) {
+    // Implementación del movimiento
+    if (Keyboard::isKeyPressed(Keyboard::W) && sprite.getPosition().y > -485) {
         sprite.move(0, -vel);
         view.move(0, -vel);
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && sprite.getPosition().y < 485) {
+    if (Keyboard::isKeyPressed(Keyboard::S) && sprite.getPosition().y < 485) {
         sprite.move(0, vel);
         view.move(0, vel);
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && sprite.getPosition().x < 735) {
+    if (Keyboard::isKeyPressed(Keyboard::D) && sprite.getPosition().x < 735) {
         sprite.move(vel, 0);
         view.move(vel, 0);
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && sprite.getPosition().x > -735) {
+    if (Keyboard::isKeyPressed(Keyboard::A) && sprite.getPosition().x > -735) {
         sprite.move(-vel, 0);
         view.move(-vel, 0);
     }
 }
 
-void Player::Aim(sf::RenderWindow &rw) {
-    sprite.setRotation(atan2(sf::Mouse::getPosition(rw).y - rw.mapCoordsToPixel(sprite.getPosition()).y, 
-                              sf::Mouse::getPosition(rw).x - rw.mapCoordsToPixel(sprite.getPosition()).x) * 180 / 3.14159265f);
+void Player::apuntar(RenderWindow &rw) {
+    sprite.setRotation(atan2(Mouse::getPosition(rw).y - rw.mapCoordsToPixel(sprite.getPosition()).y,
+                              Mouse::getPosition(rw).x - rw.mapCoordsToPixel(sprite.getPosition()).x) * 180 / 3.14159265f);
 }
 
-std::pair<bool, sf::Vector2f> Player::AnswerShoot(sf::RenderWindow &rw) {
-    sf::Vector2f dir = sf::Vector2f(sf::Mouse::getPosition(rw)) - sf::Vector2f(rw.mapCoordsToPixel(sprite.getPosition()));
-    sf::Vector2f dirNorm = sf::Vector2f(dir.x / sqrt(pow(dir.x, 2) + pow(dir.y, 2)), 
-                                             dir.y / sqrt(pow(dir.x, 2) + pow(dir.y, 2)));
+pair<bool, Vector2f> Player::AnswerShoot(RenderWindow &rw) {
+    Vector2f direc = Vector2f(Mouse::getPosition(rw)) - Vector2f(rw.mapCoordsToPixel(sprite.getPosition()));
+    Vector2f dirNorm = Vector2f(direc.x / sqrt(pow(direc.x, 2) + pow(direc.y, 2)), direc.y / sqrt(pow(direc.x, 2) + pow(direc.y, 2)));
 
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && cadence >= 8) {
-        cadence = 1;
-        sprite.setTextureRect(sf::IntRect(0, 0, 250, 115));
-        return {true, dirNorm};
-    }
-    cadence++;
-    if (cadence == 4) sprite.setTextureRect(sf::IntRect(0, 115, 250, 115));
-
-    return {false, dirNorm};
-}
-
-float Player::GetRotation() {
-    return sprite.getRotation();
-}
-
-sf::Vector2f Player::GetPosition() const {
-    return sprite.getPosition();
-}
-
-void Player::draw(sf::RenderTarget &rt, sf::RenderStates rs) const ```cpp
-{
-    rt.draw(sprite, rs);
+    // Implementación de la lógica de disparo
+    return {false, dirNorm}; // Placeholder, deberías implementar la lógica de disparo
 }
