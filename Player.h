1aeffda279ed7d1bba5a021de4427a8e620343c7
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-11-26 17:23:37 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,24 +1,15 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include <SFML/Graphics.hpp>
-#include "GameObject.h"
+#include "Character.h"
 
-class Player : public GameObject {
+class Player : public Character {
 public:
-    Player(sf::Texture&);
-    void Update(sf::RenderWindow&, sf::View&) override; // Implementación del método Update
-    std::pair<bool, sf::Vector2f> AnswerShoot(sf::RenderWindow&);
+    Player(Texture&);
+    void Update(RenderWindow &rw, View &view) override; // Implementación del método
+    void apuntar(RenderWindow &rw);
+    pair<bool, Vector2f> AnswerShoot(RenderWindow &rw);
     float GetRotation();
-    sf::Vector2f GetPosition() const override; // Implementación del método GetPosition
-    virtual void draw(sf::RenderTarget&, sf::RenderStates) const override;
-
-private:
-    sf::Sprite sprite;
-    float vel;
-    int cadence = 8;
-    void Movement(sf::View&);
-    void Aim(sf::RenderWindow&);
 };
 
-#endif // PLAYER_H
+#endif
